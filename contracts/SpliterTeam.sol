4d5c5824df3996c361f7c8b4db7136bfbc4fb785
pymachado <pyuris22@gmail.com>
2022-01-23 02:54:47 -0500

    added SpliterTeam

--- contracts/SpliterTeam.sol
+++ contracts/SpliterTeam.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./ABDKMath64x64.sol";
 
 contract SpliterTeam is Ownable{
   uint[3] setCategoryBalance;
@@ -10,15 +11,18 @@ contract SpliterTeam is Ownable{
 
   uint periodVestingTime;
   uint totalReleased;
-  
+   
   struct TEMPLATE {
     address beneficiary;
     uint amount;
+    uint category;
     uint vestingTime;
+    uint dailyIssue;
+    uint startDate;
+    uint lastClaim;
   }
 
-  mapping (uint => mapping(address => uint)) setBeneficiaryByCategory;
-  mapping (address => uint) vestingTimes;
+  mapping (address => TEMPLATE) user;
   address valeria; 
   IERC20 VAL;
 
@@ -33,34 +37,64 @@ contract SpliterTeam is Ownable{
 
   function insertListOf(uint category, TEMPLATE[] memory _subwhitelist) external onlyOwner returns(bool) {
       for (uint8 i = 0; i <_subwhitelist.length; i++) {
-        setBeneficiaryByCategory[category][_subwhitelist[i].beneficiary] += _subwhitelist[i].amount;
+        user[_subwhitelist[i].beneficiary] = TEMPLATE(
+          _subwhitelist[i].beneficiary, 
+          _subwhitelist[i].amount, 
+          category, 
+          (block.timestamp + 540 * 1 days), 
+          ABDKMath64x64.mulu(ABDKMath64x64.inv(ABDKMath64x64.fromUInt(540)), _subwhitelist[i].amount), 
+          block.timestamp, 
+          block.timestamp);
         setCategoryAccumalated[category] += _subwhitelist[i].amount;
-        vestingTimes[_subwhitelist[i].beneficiary] = (block.timestamp + _subwhitelist[i].vestingTime) / 1 days;
         _checkAmount(setCategoryAccumalated[category], setCategoryBalance[category]);  
       }
     return true;
   }
 
-  function addProfitToBenefociary(uint category, address _beneficiary, uint _amount) external onlyOwner returns (bool){
-    setBeneficiaryByCategory[category][_beneficiary] += _amount;
-    setCategoryAccumalated[category] += setBeneficiaryByCategory[category][_beneficiary];
-    require(setCategoryAccumalated[category] <= setCategoryBalance[category], "Error: Amount added exceeds the category balance.");
+  function addProfitToBeneficiary(address _beneficiary, uint _amount) external onlyOwner returns (bool){
+    user[_beneficiary].amount += _amount;
+    setCategoryAccumalated[user[_beneficiary].category] += _amount;
+    require(setCategoryAccumalated[user[_beneficiary].category] <= setCategoryBalance[user[_beneficiary].category], "Error: Amount added exceeds the category balance.");
     return true;
   }
 
-  function subProfitToBenefociary(uint category, address _beneficiary, uint _amount) external onlyOwner returns(bool) {
-    require(_amount <= setBeneficiaryByCategory[category][_beneficiary]);
-    setBeneficiaryByCategory[category][_beneficiary] -= _amount;
-    setCategoryAccumalated[category] -= setBeneficiaryByCategory[category][_beneficiary];
+  function subProfitToBeneficiary(address _beneficiary, uint _amount) external onlyOwner returns(bool) {
+    require(_amount <= user[_beneficiary].amount);
+    setCategoryAccumalated[user[_beneficiary].category] -= _amount;
     return true;
   }
 
-  function getTotalBalanceOfDistribuition() external view returns(uint) {
+  function getTotalBalanceForDistribuition() external view returns(uint) {
     return VAL.balanceOf(address(this));
   }
 
-  function releasedOf(address beneficiary) external view returns (uint) {
-      // que pasa en el final del vesintg time, debe dar 540 
+  function accumulatedRelease() external view returns (uint) { 
+      return user[msg.sender].dailyIssue * daysPassed(user[msg.sender].startDate); 
+  }
+
+  function toRelease() external view returns(uint) {
+    return user[msg.sender].amount;
+  }
+
+  function totalClaimed() external view returns(uint) {
+    uint validDaysToPay = daysPassed(user[msg.sender].startDate) - (daysPassed(user[msg.sender].startDate) % 30);
+    return user[msg.sender].dailyIssue * validDaysToPay;
+  }
+
+  function claim() external returns(bool) {
+    require(daysPassed(user[msg.sender].startDate) > 30, "Error: Wait at least after first 30 days.");
+    uint validDaysToPay = daysPassed(user[msg.sender].lastClaim) - (daysPassed(user[msg.sender].lastClaim) % 30);
+    user[msg.sender].amount -= user[msg.sender].dailyIssue * validDaysToPay;
+    require(user[msg.sender].amount > 0, "Error: Funds had been claimed.");
+    VAL.transfer(msg.sender, user[msg.sender].dailyIssue * validDaysToPay);
+    user[msg.sender].lastClaim = block.timestamp;
+    return true;
+  }
+
+  function daysPassed(uint startDate) private view returns(uint) {
+    require(startDate <= block.timestamp);
+    require(540 - ((block.timestamp - startDate) * 1 days) > 0, "Time is over.");
+    return (block.timestamp - startDate) * 1 days;
   }
 
   function _checkAmount(uint _check, uint _amount) private pure {
